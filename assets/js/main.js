52036401394c745eddf5a8d7891dea3cc5a6692f
aaliaga-casa <a.aliaga.val@gmail.com>
2017-06-06 21:59:48 -0300

    no desaparece el menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
 
- /*var menu= document.getElementById("btn");
- menu.classList.remove("");
+var menu= document.getElementById("btn");
 
- 	menu.addEventListener("click",function(){
- 		document.getElementsByClassName("mostrar").classList.toggle("mostrar")
+ 	menu.addEventListener("click",function togg(){
+ 		if (document.getElementById("mst").style.display =="none"){
+ 			document.getElementById("mst").style.display ="block";
+ 		} else {
+ 			document.getElementById("mst").style.display="block"
+ 		}
+ 		
  });
-
-function ver(n) {
-         document.getElementById("subseccion"+n).style.display="block"
-         }
-function ocultar(n) {
-         document.getElementById("subseccion"+n).style.display="none"
-         }
-*/
